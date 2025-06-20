18364aee66cd07c7bfc2f7af02fb1d30a7047c24
Utkarsh Sharma <ricksyahrma1111@gmail.com>
2025-06-20 23:23:46 +0530

    UPdated Version

--- global.sh
+++ global.sh
@@ -4,7 +4,9 @@
 # Downloads and sources the helper_logs.sh script from remote repository
 
 # Download and source the helper script
-HELPER_SCRIPT=$(mktemp)
-curl -s https://raw.githubusercontent.com/f1forhelp/scripts/refs/heads/main/helper_logs.sh > "$HELPER_SCRIPT"
-. "$HELPER_SCRIPT"
-rm "$HELPER_SCRIPT" 
+HELPER_LOGS=$(mktemp)
+curl -s https://raw.githubusercontent.com/f1forhelp/scripts/refs/heads/main/helper_logs.sh > "$HELPER_LOGS"
+. "$HELPER_LOGS"
+rm "$HELPER_LOGS" 
+
+logk "i" "v0.0.1"
