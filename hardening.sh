64cb9a64ac32051a8f33f1daca8c6dba7b3d0c7d
Utkarsh Sharma <ricksyahrma1111@gmail.com>
2025-06-20 23:19:54 +0530

    Reduced code

--- hardening.sh
+++ hardening.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-echo "Starting hardening script... 6"
-# Pretty print logging function
-# Download and source the helper script
+# Download and source the global script
 GLOBAL_SCRIPT=$(mktemp)
 curl -s https://raw.githubusercontent.com/f1forhelp/scripts/refs/heads/main/global.sh > "$GLOBAL_SCRIPT"
 . "$GLOBAL_SCRIPT"
 rm "$GLOBAL_SCRIPT"
 
 # Example usage
-logk "info" "Starting hardening script... 1"
-logk "success" "System check completed successfully"
-logk "warning" "Some settings may need manual review"
-logk "error" "Failed to apply security policy"
+logk "i" "Starting hardening script... 2"
+logk "s" "System check completed successfully"
+logk "w" "Some settings may need manual review"
+logk "e" "Failed to apply security policy"
 logk "" "Failed to apply security policy"
--- helper_logs.sh
+++ helper_logs.sh
@@ -8,16 +8,16 @@ logk() {
     local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
     
     case $level in
-        "info")
+        "i")
             echo -e "\033[34m[INFO]\033[0m \033[90m$timestamp\033[0m $message"
             ;;
-        "success")
+        "s")
             echo -e "\033[32m[SUCCESS]\033[0m \033[90m$timestamp\033[0m $message"
             ;;
-        "warning")
+        "w")
             echo -e "\033[33m[WARNING]\033[0m \033[90m$timestamp\033[0m $message"
             ;;
-        "error")
+        "e")
             echo -e "\033[31m[ERROR]\033[0m \033[90m$timestamp\033[0m $message"
             ;;
         *)
@@ -28,8 +28,8 @@ logk() {
 
 
 # Example usage
-logk "info" "Starting hardening script..."
-logk "success" "System check completed successfully"
-logk "warning" "Some settings may need manual review"
-logk "error" "Failed to apply security policy"
-logk "" "Failed to apply security policy"
+# logk "info" "Starting hardening script..."
+# logk "success" "System check completed successfully"
+# logk "warning" "Some settings may need manual review"
+# logk "error" "Failed to apply security policy"
+# logk "" "Failed to apply security policy"
