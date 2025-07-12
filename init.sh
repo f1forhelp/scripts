75ff5a95a044cdd71470a271998cd19d2618b95e
Utkarsh Sharma <ricksyahrma1111@gmail.com>
2025-07-13 01:49:37 +0530

    0.0.29

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly VERSION="0.0.28"   
+readonly VERSION="0.0.29"   
 
 # Download and source the helper script
 HELPER_LOGS=$(mktemp)
@@ -43,19 +43,26 @@ curl -s https://raw.githubusercontent.com/f1forhelp/scripts/refs/heads/main/scri
 . "$POSTGRES_SCRIPT"
 rm "$POSTGRES_SCRIPT"
 
+NETBIRD_SCRIPT=$(mktemp)
+curl -s https://raw.githubusercontent.com/f1forhelp/scripts/refs/heads/main/script_netbird.sh > "$NETBIRD_SCRIPT"
+. "$NETBIRD_SCRIPT"
+rm "$NETBIRD_SCRIPT"
+
 logk "s" "v$VERSION"
 loge ""
 
 logk "i" "Select below options (Remember to run as non root user except Hardening One)"
 loge "1. Hardening One"
 loge "2. Hardening Two"
-loge "3. Firewall"
-loge "4. Postgres"
+loge "3. Netbird"
+loge "4. Firewall"
+loge "5. Postgres"
 read -p "Enter the option: " option
 
 case $option in
     1) script_hardening_one ;;
     2) script_hardening_two ;;
-    3) script_firewall ;;
-    4) script_postgres ;;
+    3) script_netbird ;;
+    4) script_firewall ;;
+    5) script_postgres ;;
 esac
---
+++ script_netbird.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ---- Available Options ----
+install_netbird(){
+    logk "i" "Installing netbird"
+    curl -fsSL https://pkgs.netbird.io/install.sh | sh
+    netbird up
+    logk "i" "Netbird installed"
+}
+
+install_netbird_with_ssh(){
+    logk "i" "Installing netbird with ssh"
+    curl -fsSL https://pkgs.netbird.io/install.sh | sh
+    netbird up --allow-server-ssh
+    logk "i" "Netbird installed"
+}
+
+script_netbird(){
+    logk "i" "Select the option for netbird"
+    loge "1. Install netbird"
+    loge "2. Install netbird with ssh"
+    read -p "Enter your choice: " choice
+    case $choice in
+        1) install_netbird ;;
+        2) install_netbird_with_ssh ;;
+        *) logk "e" "Invalid choice" ;;
+    esac
+}
