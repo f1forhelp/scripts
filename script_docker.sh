c54b8f4b2d8a69c0d0690e0bbae8a0520578a938
utkarsh sharma <rickysharma1111@gmail.com>
2025-10-01 22:33:15 +0530

    Enhance Docker installation script with new features
    - Updated the `install_docker` function to include Docker's official GPG key and repository setup for Debian.
    - Removed the separate `install_docker_compose` function and integrated Docker Compose installation into the main installation process.
    - Modified prompts for advertising leader and promoting nodes to use clearer terminology.
    - Added new functions for logging in and out of GitHub Container Registry (ghcr).
    - Updated the options in the main script to reflect the new functionality.

--- script_docker.sh
+++ script_docker.sh
@@ -1,28 +1,52 @@
 # ---- Functions ----
 install_docker() {
     logk "i" "Installing docker..."
-    sudo apt-get install -y docker.io
-    logk "i" "Docker installed"
-}
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-install_docker_compose() {
-    logk "i" "Installing docker compose..."
-    sudo apt-get install -y docker-compose
-    logk "i" "Docker compose installed"
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl enable --now docker
+    sudo systemctl status docker
+
+    logk "i" "Docker installed"
 }
 
 advertise_leader() {
     logk "i" "Advertising leader..."
-    read -p "Enter the IP address to advertise: " ip_address_advertise
-    docker swarm init --advertise-addr $ip_address_advertise
+    read -p "Enter the private IP address to advertise: " private_ip
+    docker swarm init --advertise-addr $private_ip
     logk "i" "Leader advertised"
 }
 
 promote_node_to_manager() {
-    logk "i" "Promoting node to manager..."
-    read -p "Enter the node ID/Hostname: " node_id
-    docker node promote $node_id
+    logk "i" "Promoting node to manager run this command on leader node"
+    read -p "Enter the hostname to promote: " hostname
+    docker node promote $hostname
     logk "i" "Node promoted to manager"
+}
+
+docker_login_ghcr() {
+    logk "i" "Logging in to ghcr..."
+    read -p "Enter the username: " username
+    read -s -p "Enter the password: " password; echo
+    docker login ghcr.io -u $username -p $password
+    logk "i" "Logged in to ghcr"
+}
+
+docker_logout_ghcr() {
+    logk "i" "Logging out from ghcr..."
+    docker logout ghcr.io
+    logk "i" "Logged out from ghcr"
 }
 
 # ---- Options ----
@@ -32,18 +56,24 @@ script_docker() {
     logk "i" "Starting docker script..."
     logk "i" "Select the option for docker"
     loge "1. Install docker"
-    loge "2. Install docker compose"
-    loge "3. Advertise leader"
-    loge "4. Promote node to manager"
+    loge "2. Advertise leader"
+    loge "3. Promote node to manager"
+    loge "4. Login to ghcr"
+    loge "5. Logout from ghcr"
     read -p "Enter your choice: " choice
     case $choice in
         1) install_docker ;;
-        2) install_docker_compose ;;
-        3) advertise_leader ;;
-        4) promote_node_to_manager ;;
+        2) advertise_leader ;;
+        3) promote_node_to_manager ;;
+        4) docker_login_ghcr ;;
+        5) docker_logout_ghcr ;;
         *) logk "e" "Invalid choice" ;;
     esac
 }
 
 # --- Basic Commands ---
 # docker node ls
+
+# https://www.youtube.com/watch?v=nmas_zbcMeU&t=88s
+# https://www.youtube.com/watch?v=RgZyX-e6W9E
+# https://www.youtube.com/watch?v=_YsPt7dIvqU
